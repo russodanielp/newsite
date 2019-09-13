78496825e81890d7ea334f8d0298f1cd18434b8a
Daniel P. Russo <russodanielp@gmail.com>
2019-09-13 15:02:11 -0400

    fixed typo and broken link

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "Daniel P. Russo"  # (translatable)
 BLOG_TITLE = "Drug Discovery in Python"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://www.danielprusso.com/"
+SITE_URL = "http://www.danielprusso.com/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
